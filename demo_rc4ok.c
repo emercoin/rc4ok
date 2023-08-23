2c8af3b0e08021e9c38031692589a7c505198dbc
Oleg Khovayko <khovayko@gmail.com>
2023-08-23 12:18:26 -0400

    Add algotest

--- demo_rc4ok.c
+++ demo_rc4ok.c
@@ -6,7 +6,16 @@
 #include <rc4ok.h>
 
 /*-----------------------------------------------------------------------------*/
-// Add entropy to rc4ok PGNG, every ~1s
+// Test key and output reference vector
+const char G_algotest_key[] = "rc4ok-is-the-best";
+const uint8_t G_algotest_refvec[] = {
+    0x10, 0x4a, 0x1e, 0x8e, 0x59, 0xb3, 0x03, 0x67, 0x99, 0x33, 0x96, 0xb4, 0x60, 0x60, 0x16, 0x5a,
+    0x7f, 0xd9, 0xe2, 0x71, 0xe8, 0x6e, 0x07, 0xf5, 0xa5, 0x18, 0xee, 0x40, 0x81, 0x96, 0x58, 0x4c,
+    0x35, 0x67, 0x50, 0xbd, 0x3f, 0x17, 0x87, 0x40, 0x6d, 0x0f, 0x06, 0xcd, 0x8a, 0x0e, 0x82, 0x76,
+    0x80, 0xba, 0xf8, 0x23, 0x2d, 0xf4, 0x6a, 0xcc, 0xfa, 0xce, 0x40, 0x1a, 0x95, 0x50, 0xe6, 0x92
+};
+
+/*-----------------------------------------------------------------------------*/
 void *entropy(void *p) {
     uint16_t x = 0;
     rc4ok *q;
@@ -20,13 +29,19 @@ void *entropy(void *p) {
 
 /*-----------------------------------------------------------------------------*/
 int main(int argc, char **argv) {
+    rc4ok prng;
     if(argc < 4) {
-        fprintf(stderr, "RC4OK bytestream PRNG\n"
+        if(argc == 2 && strcmp(argv[1], "algotest") == 0) {
+            rc4ok_ksa(&prng, (const uint8_t *)G_algotest_key, strlen(G_algotest_key));
+            uint8_t buf[sizeof(G_algotest_refvec)];
+            rc4ok_prng(&prng, buf, sizeof(buf));
+            printf("%s: Algotest %s\n", argv[0], memcmp(G_algotest_refvec, buf, sizeof(G_algotest_refvec))? "FAILED!" : "Passed");
+        } else
+            fprintf(stderr, "RC4OK bytestream PRNG\n"
                 "Run:\n\t%s gen_size_mb=\\d+ prng-seed-key=\\s+ use_entropy={-1(no output),0(no entropy),1(add entropy)}\n", argv[0]);
         return 0;
     }
 
-    rc4ok prng;
     uint32_t num_mb = atoi(argv[1]); // Megabytes to out
     rc4ok_ksa(&prng, (const uint8_t *)argv[2], strlen(argv[2]));
     int use_entropy = atoi(argv[3]);
