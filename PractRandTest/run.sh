5f8d2a64cd8074596656d21c839472a5a293d6f7
Oleg Khovayko <khovayko@gmail.com>
2023-09-04 12:16:25 -0400

    Add multithread for PractRand

--- PractRandTest/run.sh
+++ PractRandTest/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 renice -n 10 $$
 DATE=`date`
-# SEED="The_best_rc4ok_second__PractRand_test"
-SEED="0"
+SEED="The_best_rc4ok_second__PractRand_test_8ff14b42bc30ddbded0fae8a780bdc9e43c51247"
 echo "-------- $DATE --- $SEED ------" >> practrand_out.txt
-./demo_rc4ok 1000000000 $SEED 0 | ./RNG_test stdin >>practrand_out.txt &
+./demo_rc4ok 1000000000 $SEED 0 | ./RNG_test stdin -multithreaded >>practrand_out.txt &
 tail -f practrand_out.txt
+
